a782d05dd88b6c0a743a437f15b47d0652dc5a81
Alex Kates <alex.kates@croissant.com>
2024-01-31 08:47:12 -0500

    Add fadeIn animation and update UI components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import CreateNextApp from "@/components/create-next-app";
 import { Button } from "@/components/ui/button";
+import { cn, fadeIn } from "@/lib/utils";
 import { GitHubLogoIcon, VercelLogoIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 import { Metadata } from "next/types";
@@ -34,24 +35,26 @@ export async function generateMetadata() {
 export default async function Page() {
   return (
     <main className="flex flex-col gap-4 pb-12 pt-4 text-center items-center sm:gap-8 sm:py-20">
-      <div className="flex flex-col gap 2">
+      <section className={cn(fadeIn, "animation-delay-200 flex flex-col gap 2")}>
         <h1 className="text-4xl font-bold sm:text-7xl">hashnode-next</h1>
         <h2 className="text-lg font-light text-muted-foreground sm:text-xl">The fastest way to go headless with Hashnode</h2>
-      </div>
-      <h3 className="text-lg sm:text-xl">
-        <div>Beautifully simple Hashnode starter-kit</div>
-        <div>
-          powered by&nbsp;
-          <Link className="underline underline-offset-2" href="https://nextjs.org/" target="_blank">
-            Next.js
-          </Link>
-          &nbsp;and&nbsp;
-          <Link className="underline underline-offset-2" href="https://ui.shadcn.com/" target="_blank">
-            shadcn/ui
-          </Link>
-        </div>
-      </h3>
-      <div className="flex items-center gap-8">
+      </section>
+      <section className={cn(fadeIn, "animation-delay-400")}>
+        <span className="text-lg sm:text-xl">
+          <div>Beautifully simple Hashnode starter-kit</div>
+          <div>
+            powered by&nbsp;
+            <Link className="underline underline-offset-2" href="https://nextjs.org/" target="_blank">
+              Next.js
+            </Link>
+            &nbsp;and&nbsp;
+            <Link className="underline underline-offset-2" href="https://ui.shadcn.com/" target="_blank">
+              shadcn/ui
+            </Link>
+          </div>
+        </span>
+      </section>
+      <section className={cn(fadeIn, "flex items-center gap-8 animation-delay-600")}>
         <Button asChild size="lg">
           <Link
             href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Falexkates%2Fhashnode-next&env=HASHNODE_API_KEY,HASHNODE_API_URL,HASHNODE_HOST&envDescription=Hashnode%20docs&envLink=https%3A%2F%2Fapidocs.hashnode.com%2F&project-name=blog&repository-name=blog"
@@ -66,8 +69,10 @@ export default async function Page() {
             <GitHubLogoIcon className="mr-2 h-4 w-4" /> GitHub
           </Link>
         </Button>
-      </div>
-      <CreateNextApp />
+      </section>
+      <section className={cn(fadeIn, "animation-delay-800")}>
+        <CreateNextApp />
+      </section>
     </main>
   );
 }
